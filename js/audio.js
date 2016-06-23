26d376f9358659bf93c476d34bed58f69e9efb26
s15359ys <s15359ys@sfc.keio.ac.jp>
2016-06-23 11:32:46 +0900

    関数play,関数pauseを定義
    関数play:audioを再生させる関数
    関数pause:audioを一時停止させる関数

--- js/audio.js
+++ js/audio.js
@@ -1,4 +1,12 @@
 console.log("start audio.js");
+var html, body, audio;
+html = document.children[0];
+body = html.children[1];
+audio = body.children[1];
+
+audio.play();
+audio.pause();
+
 
 function rewind10sec(sec) {
  var rewind = sec-10;
@@ -8,4 +16,11 @@ function rewind10sec(sec) {
 function forward10sec(sec) {
  var forward = sec+10;
  audio.currentTime = forward;
+}
+
+function play(){
+    audio.play();
+}
+function pause(){
+    audio.pause();
 }
